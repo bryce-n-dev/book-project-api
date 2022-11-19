bc1394a8c968ff8443a21edc646b0c68c8b5614d
Bryce Nichol <bryce.n.dev@icloud.com>
2022-11-19 16:25:36 -0500

    POST user book - add book to shelf / user's book collection

--- crud.py
+++ crud.py
@@ -24,6 +24,9 @@ def get_favourites(db: Session, user_id: str):
 def get_books(db: Session, search: str):
     return db.query(models.BookInfo).filter(models.BookInfo.title.contains(search)).all()
 
+def get_book_by_isbn(db: Session, isbn: str):
+    return db.query(models.BookInfo).filter(models.BookInfo.isbn == isbn).first()
+
 # Get shelf
 def get_user_shelf(db: Session, user_id: str, shelf_id: int):
     return db.query(models.UserBook.isbn, models.BookInfo.title, models.BookInfo.cover_url).join(models.BookInfo, models.UserBook.isbn == models.BookInfo.isbn).filter(models.UserBook.user_id == user_id).filter(models.UserBook.shelf_id == shelf_id).all()
@@ -33,13 +36,17 @@ def get_user_book(db: Session, user_id: str, isbn: str):
     return db.query(models.UserBook.isbn,models.UserBook.rating, models.UserBook.rating, models.UserBook.is_favourite, models.UserBook.review,models.BookInfo.title, models.BookInfo.cover_url, models.BookInfo.blurb, models.BookInfo.year,models.Author.name, models.Genre.genre).join(models.BookInfo, models.UserBook.isbn == models.BookInfo.isbn).join(models.Author, models.BookInfo.author_id == models.Author.author_id).join(models.Genre, models.Genre.genre_id == models.BookInfo.genre_id).filter(models.UserBook.user_id == user_id).filter(models.UserBook.isbn == isbn).first()
 
 # Can be used to update favourite or update shelf ID
-def update_book(db: Session, user_id: str, isbn: Union[int, None] = None, favourite: Union[bool, None] = None):
+def update_user_book(db: Session, user_id: str, isbn: Union[int, None] = None, favourite: Union[bool, None] = None):
     pass
 
 # Remove book from bookshelf (delete entry from DB - user_book)
-def delete_book_from_shelf(db: Session, user_id: str, isbn: str):
+def delete_user_book(db: Session, user_id: str, isbn: str):
     pass
 
 # Add book to bookshelf (add entry to DB - user_book)
-def add_book_to_shelf(db: Session, book: schemas.Book, user_id: int, isbn: str):
-    pass
+def create_user_book(db: Session, user_book: schemas.UserBookCreate, user_id: str):
+    db_user_book = models.UserBook(isbn=user_book.isbn, user_id=user_id, shelf_id=user_book.shelf_id)
+    db.add(db_user_book)
+    db.commit()
+    db.refresh(db_user_book)
+    return db_user_book
